c8f42a55f7b75a26235e5c37e29bb09434d8342c
Jose Calderon <josephwantri@gmail.com>
2025-01-03 00:40:24 -0500

    add roulette component

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import About from './components/about/about';
+import { Roulette } from './components/roulette/Roulette';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/roulette" element={<Roulette />} />
       <Route path="/about" element={<About />} />
     </Routes>
   );
