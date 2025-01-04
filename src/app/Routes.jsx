01722ed4021b24b25f68d8036dea7cea6fc9f144
Estefano-Lostaunau <u202211742@upc.edu.pe>
2025-01-04 00:13:14 -0500

    feat(project): Update project

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/home';
 import About from './components/about/about';
+import NotFound from './components/notfound/notfound'; // Asegúrate de tener un componente NotFound
 import { Roulette } from './components/roulette/Roulette';
 import AdminPanel from './components/admin-panel/AdminPanel';
 
@@ -12,6 +13,8 @@ function AppRoutes() {
       <Route path="/roulette" element={<Roulette />} />
       <Route path="/panel" element={<AdminPanel />} />
       <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+
     </Routes>
   );
 }
