5eb43d4a4d5b68af4037535ad2166d3653882138
Estefano-Lostaunau <u202211742@upc.edu.pe>
2025-01-07 16:42:11 -0500

    feat(login): update link

--- src/app/components/login/login.jsx
+++ src/app/components/login/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
-  const from = location.state?.from?.pathname || '/roulette';
+  const from = location.state?.from?.pathname || '/';
 
   const handleLogin = async (e) => {
     e.preventDefault();
